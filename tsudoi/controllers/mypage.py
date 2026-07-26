ffc5fc8988d1f066306d014d4d538207dfdf3012
Inoue0422 <o9045628081@gmail.com>
2026-07-27 07:13:57 +0900

    マイページとユーザー設定画面の遷移を実装

--- tsudoi/controllers/mypage.py
+++ tsudoi/controllers/mypage.py
@@ -1,7 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+# auth_gurd.py実装後に追加
+# from util.auth_guard import login_required
 
 mypage_bp = Blueprint("mypage", __name__)
 
+# マイページ
 @mypage_bp.route("/mypage", methods=["GET"])
+# auth_gurd.py実装後に追加
+# @login_required
 def mypage_view():
-    return render_template("mypage/mypage.html")
+    return render_template("mypage/index.html")
+
+# ユーザー編集画面表示
+@mypage_bp.route("/user/edit", methods=["GET"])
+# auth_guard.py実装後に追加
+# @loguin_required
+def user_edit_view():
+    return render_template("mypage/settings.html")
+
+# ユーザー編集処理
+@ mypage_bp.route("/user", methods=["POST"])
+# auth_guard.py実装後に追加
+# @login_required
+def user_edit_process():
+    errors =[]
+    
+    # TODO: 入力チェックを実装する
+    # 例：
+    # if ユーザー名が空欄:
+    #   errors.append("ユーザー名を入力してください")
+
+    # エラーがある場合
+    if errors:
+        return render_template("mypage/settings.html", error_masseges=errors) # errorsリストの内容をerror_massegesとしてHTMLで利用できるようにする
+
+    # TODO: Modelを使ってユーザー情報を更新する
+
+    # 更新成功後はマイページへ移動
+    return redirect(url_for("mypage.mypage_view"))
