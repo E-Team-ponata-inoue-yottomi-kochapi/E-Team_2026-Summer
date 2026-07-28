c1ac944e4dbb1f599dc958d4f9203bd4c6673436
Inoue0422 <o9045628081@gmail.com>
2026-07-29 07:07:56 +0900

    集計画面の表示処理の骨組みを追加

--- tsudoi/app.py
+++ tsudoi/app.py
@@ -4,12 +4,14 @@ from flask_wtf.csrf import CSRFProtect
 from config import settings
 from controllers.auth import auth_bp
 from controllers.mypage import mypage_bp
+from controllers.application import application_bp
 
 app = Flask(__name__)
 app.secret_key = settings.SECRET_KEY
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(mypage_bp)
+app.register_blueprint(application_bp)
 
 # CSRF対策
 csrf = CSRFProtect(app)
