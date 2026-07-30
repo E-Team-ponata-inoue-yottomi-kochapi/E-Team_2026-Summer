1d2e93a25c6d2142c1ab73f071eb24af11520519
yosikiti <y.yosikiti54161000@gmail.com>
2026-07-30 18:41:02 +0900

    Update redirect to use 'auth.login_view'

--- tsudoi/controllers/household.py
+++ tsudoi/controllers/household.py
@@ -7,7 +7,7 @@ def login_required(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         if session.get('user_id') is None:
-            return redirect(url_for('login_view'))
+            return redirect(url_for('auth.login_view'))
         return func(*args, **kwargs)
     return wrapper
 
