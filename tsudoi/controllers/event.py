276d8517488fb2fe6ba06a22ee2cd533ba0a2eb1
小坪太一 <dnt.spe918@gmail.com>
2026-07-31 22:34:09 +0900

    スケルトン実装Ver1

--- tsudoi/controllers/event.py
+++ tsudoi/controllers/event.py
@@ -2,14 +2,35 @@ from flask import Blueprint, session, render_template, redirect, url_for, reques
 
 event_bp = Blueprint("event", __name__, url_prefix="/events")
 
+
+# イベント一覧表示画面
 @event_bp.route("/", methods=["GET"])
 def list_view():
     # if session.get("user_id") is None:
     #     return redirect(url_for('auth.login_view'))
     return render_template("event/event_list.html")
 
+
+# イベント詳細表示画面
 @event_bp.route("/<string:id>", methods=["GET"])
 def detail_view(id):
     # if session.get("user_id") is None:
     #     return redirect(url_for('auth.login_view'))
     return render_template("event/event_detail.html")
+
+
+# イベント作成画面表示
+@event_bp.route("/new", methods=["GET"])
+# @login_required
+def new_view():
+ 
+    return render_template("event/event_form.html")
+
+
+# イベント作成処理
+@event_bp.route("/", methods=["POST"])
+# @login_required
+def create_process():
+    #仮ID
+    event_id = 1
+    return redirect(url_for("event.detail_view", id=event_id))
