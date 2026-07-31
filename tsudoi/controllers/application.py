b7249b884da032ac04db536f8579c7e77bca946e
小坪太一 <dnt.spe918@gmail.com>
2026-07-31 22:42:14 +0900

    スケルトン実装fix a bit

--- tsudoi/controllers/application.py
+++ tsudoi/controllers/application.py
@@ -27,7 +27,7 @@ def summary_view(event_id):
 
 #申込フォーム表示/参加メンバー選択画面
 @application_bp.route('/events/<string:event_id>/', methods=["GET"])
-@login_required
+# @login_required
 def apply_view(event_id):
     members = [
         {"id": 1, "name": "田中太郎"},
@@ -38,7 +38,7 @@ def apply_view(event_id):
   
 #申し込み内容確認画面
 @application_bp.route('/events/<string:event_id>/confirm', methods=["POST"])
-@login_required
+# @login_required
 def apply_confirmation_view(event_id):
     # フォームで選択されたmember_idsを受け取り、まだ保存せず画面表示のみ
     event = {"id": event_id, "name": "サンプルイベント"}
@@ -54,7 +54,7 @@ def apply_confirmation_view(event_id):
   
 #申し込み確定処理
 @application_bp.route('/events/<string:event_id>/create', methods=["POST"])
-@login_required
+# @login_required
 def apply_create_process(event_id):
     application_id = 1  # 仮:保存後に発行されたID
     flash("申し込みが完了しました")
@@ -63,7 +63,7 @@ def apply_create_process(event_id):
   
 #申し込み内容編集画面
 @application_bp.route('/applications/<int:id>/edit', methods=["GET"])
-@login_required
+# @login_required
 def apply_edit_view(id):
     application = {"id": id}
     members = [
@@ -75,14 +75,14 @@ def apply_edit_view(id):
   
 #申し込み編集処理
 @application_bp.route('/applications/<int:id>/edit', methods=["POST"])
-@login_required
+# @login_required
 def apply_edit_process(id):
     return redirect(url_for('mypage.mypage_view'))
 
   
 #キャンセル処理→マイページに遷移
 @application_bp.route('/applications/<int:id>/cancel',methods=["POST"])
-@login_required
+# @login_required
 def apply_cancel_process(id):
     return redirect(url_for('mypage.mypage_view'))
 
--- tsudoi/controllers/household.py
+++ tsudoi/controllers/household.py
@@ -13,7 +13,7 @@ def login_required(func):
 
 #家族一覧ページ表示
 @household_bp.route('/', methods=["GET"])
-@login_required
+# @login_required
 def household_list_view():
     members = [
         {"id": 1, "name": "田中太郎"},
@@ -23,18 +23,18 @@ def household_list_view():
 
 #家族追加処理→家族一覧表示
 @household_bp.route('/member/create', methods=["POST"])
-@login_required
+# @login_required
 def member_create_process():
     return redirect(url_for('household.household_list_view'))
 
 #家族情報編集処理
 @household_bp.route('/member/<int:id>/edit', methods=["POST"])
-@login_required
+# @login_required
 def member_edit_process(id):
     return redirect(url_for('household.household_list_view'))
 
 #メンバー削除処理
 @household_bp.route('/member/<int:id>/delete', methods=["POST"])
-@login_required
+# @login_required
 def member_delete_process(id):
     return redirect(url_for('household.household_list_view'))
