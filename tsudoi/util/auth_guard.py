7666852bd06db3b8058a69a08118fbe3a4d28ebb
kochapi <chiaki.life555@gmail.com>
2026-08-10 11:45:01 +0900

    feat: /tsudoi/util/auth_guard.pyに@login_requiredを移設

--- tsudoi/util/auth_guard.py
+++ tsudoi/util/auth_guard.py
@@ -1,5 +1,14 @@
 # python標準ライブラリ / Flaskの機能
 from functools import wraps
-from flask import g, session, abort
+from flask import g, session, abort, redirect, url_for
 
 # 独自のmodel関数
+
+# ログイン権限デコレータ
+def login_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if session.get('user_id') is None:
+            return redirect(url_for('auth.login_view'))
+        return func(*args, **kwargs)
+    return wrapper
