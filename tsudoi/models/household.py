0de316291b09168bb060c12fd292d8f3be5943d2
Inoue0422 <o9045628081@gmail.com>
2026-08-13 21:19:59 +0900

    F-028 第2段階 マイページに実データを表示

---
+++ tsudoi/models/household.py
@@ -0,0 +1,15 @@
+import pymysql
+from util.db import get_connection
+
+def find_household_by_leader_id(user_id):
+    conn = get_connection()
+    try:
+        with conn.cursor() as cursor:
+            sql = "SELECT id, leader_id FROM households WHERE leader_id = %s"
+            cursor.execute(sql, (user_id,))
+            household = cursor.fetchone()
+            return household
+    except pymysql.MySQLError:
+        raise
+    finally:
+        conn.close()
