91fca4ebce57d55b30364b90066989dc1be154ec
Pontazm <dnt.spe918@gmail.com>
2026-08-15 03:10:09 +0900

    Enhance find_household_by_leader_id with TODOs
    Add TODO comments for code refactoring and logical deletion.

--- tsudoi/models/household.py
+++ tsudoi/models/household.py
@@ -1,10 +1,11 @@
 import pymysql
 from util.db import get_connection
-
+# TODO: find_household_by_userと重複してる。#64マージ後に統一する
 def find_household_by_leader_id(user_id):
     conn = get_connection()
     try:
         with conn.cursor() as cursor:
+      # TODO: deleted_at IS NULL が抜けてる。論理削除済みの世帯もヒットしてしまう
             sql = "SELECT id, leader_id FROM households WHERE leader_id = %s"
             cursor.execute(sql, (user_id,))
             household = cursor.fetchone()
