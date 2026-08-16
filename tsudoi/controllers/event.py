1f4e608adc721e3be2b724fafb37d66e41dee78a
小坪太一 <dnt.spe918@gmail.com>
2026-08-17 08:06:41 +0900

    イベント作成機能第２〜第３段階まで実装

--- tsudoi/controllers/event.py
+++ tsudoi/controllers/event.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, session, render_template, redirect, url_for, request, abort
-from models.event import get_open_events
+from models.event import get_open_events, create_event
 from services.event import get_event_detail
 import pymysql
 from util.auth_guard import login_required
@@ -43,7 +43,7 @@ def detail_view(event_id):
 
 # イベント作成画面表示
 @event_bp.route("/new", methods=["GET"])
-# @login_required
+@login_required
 def new_view():
  
     return render_template("event/event_form.html")
@@ -51,7 +51,24 @@ def new_view():
 
 # イベント作成処理
 @event_bp.route("/", methods=["POST"])
-# @login_required
+@login_required
 def create_process():
-
+    event_id = create_event(
+        owner_id=session['user_id'],
+        title=request.form.get('title'),
+        start_at=request.form.get('start_at'),
+        place=request.form.get('place'),
+        address=request.form.get('address'),
+        capacity=request.form.get('capacity') or None,
+        deadline=request.form.get('deadline') or None,
+        description=request.form.get('description'),
+        items_to_bring=request.form.get('items_to_bring'),
+        schedule=request.form.get('schedule'),
+        hold_condition=request.form.get('hold_condition'),
+        cancellation_policy=request.form.get('cancellation_policy'),
+        emergency_contact=request.form.get('emergency_contact'),
+        payment_method=request.form.get('payment_method'),
+        payment_deadline=request.form.get('payment_deadline') or None,
+        status='公開',
+    )
     return redirect(url_for("event.detail_view", event_id=event_id))
