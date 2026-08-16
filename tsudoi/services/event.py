d65257c8d836c26029ffc3e7711d9a3b086d35d5
小坪太一 <dnt.spe918@gmail.com>
2026-08-16 09:01:32 +0900

    find_event_by_idへのリネーム忘れをfixed

--- tsudoi/services/event.py
+++ tsudoi/services/event.py
@@ -1,7 +1,7 @@
-from models.event import find_open_event_by_id, get_owner_by_event_id, get_fee_rules_by_event_id
+from models.event import find_event_by_id, get_owner_by_event_id, get_fee_rules_by_event_id
 
 def get_event_detail(event_id):
-    event = find_open_event_by_id(event_id)
+    event = find_event_by_id(event_id)
     if event is None:
         return None
     owner = get_owner_by_event_id(event_id)
